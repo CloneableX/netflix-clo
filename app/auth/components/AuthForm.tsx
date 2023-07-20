885b65a7a86e484930307e85fd26aaddc8f07665
Cloneable <cloneable@163.com>
2023-07-20 17:52:20 +0800

    feat: google & github oauth

--- app/auth/components/AuthForm.tsx
+++ app/auth/components/AuthForm.tsx
@@ -5,6 +5,8 @@ import {useCallback, useState} from "react";
 import axios from "axios";
 import {signIn} from "next-auth/react";
 import {useRouter} from "next/navigation";
+import {FcGoogle} from "react-icons/fc";
+import {FaGithub} from "react-icons/fa";
 
 export const AuthForm = () => {
   const router = useRouter();
@@ -86,6 +88,40 @@ export const AuthForm = () => {
                 className="w-full bg-red-600 text-white mt-10 py-3 rounded-md hover:bg-red-700 transition">
           {variant === 'login' ? 'Login' : 'Sign up'}
         </button>
+        <div className="flex flex-row items-center justify-center gap-4 m-8">
+          <div
+            onClick={() => signIn('google', { callbackUrl: '/' })}
+            className="
+                w-10
+                h-10
+                bg-white
+                rounded-full
+                flex
+                justify-center
+                items-center
+                cursor-pointer
+                hover:opacity-80
+                transition
+              ">
+            <FcGoogle size={30} />
+          </div>
+          <div
+            onClick={() => signIn('github', { callbackUrl: '/' })}
+            className="
+                w-10
+                h-10
+                bg-white
+                rounded-full
+                flex
+                justify-center
+                items-center
+                cursor-pointer
+                hover:opacity-80
+                transition
+              ">
+            <FaGithub size={30} />
+          </div>
+        </div>
         <p className="text-neutral-500 mt-12">
           {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
           <span onClick={toggleVariant} className="text-white ml-1 cursor-pointer hover:underline">
